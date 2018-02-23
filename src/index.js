a622b26ffedba5e0f9ed3dc0d95144aa27961e3b
naresh ganji <naresh25ganji@gmail.com>
2018-02-23 08:32:44 -0500

    add aphrodite

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Hello from './components/Hello';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    <Hello name="TypeScript" enthusiasmLevel={10} />,
-    document.getElementById('root') as HTMLElement
-  );
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
